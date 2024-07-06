3e8561c87bfab3785e33a88b85c4133f8745f766
Hiroki Kondo <kompiro@gmail.com>
2024-07-06 23:02:19 +0900

    fin #14

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/techschool/simplebank/db/sqlc"
 )
 
@@ -16,12 +18,18 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.CreateAccount)
 	router.GET("/accounts/:account_id", server.GetAccount)
 	router.GET("/accounts", server.ListAccount)
 
 	router.GET("/entries/:id", server.GetEntry)
 	router.GET("/accounts/:account_id/entries", server.ListEntry)
+
+	router.POST("/transfers", server.CreateTransfer)
 
 	server.router = router
 	return server
