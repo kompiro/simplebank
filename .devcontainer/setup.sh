6067fb48a727cf7d2d8486295db0073b988f85a7
Hiroki Kondo <kompiro@gmail.com>
2024-07-27 18:08:50 +0900

    feat: Setup by packagecloud.io

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,7 +6,8 @@ set -eux
 sudo cp .devcontainer/welcome.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 
 # setup migrate command
-go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+curl -s https://packagecloud.io/install/repositories/golang-migrate/migrate/script.deb.sh | sudo bash
+sudo install migrate
 
 # setup sqlc
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
