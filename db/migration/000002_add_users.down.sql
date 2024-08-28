b5067ab76c6c66a2e4ce1ca2501517d9639f3af2
Hiroki Kondo <kompiro@gmail.com>
2024-08-28 22:38:37 +0900

    feat: Add sessions table migration

--- db/migration/000002_add_users.down.sql
+++ db/migration/000002_add_users.down.sql
@@ -1,7 +1,7 @@
 ALTER TABLE if exists "accounts"
-drop CONSTRAINT if EXISTS "ownwer_currency_key";
+DROP CONSTRAINT if EXISTS "ownwer_currency_key";
 
 ALTER TABLE if exists "accounts"
-drop CONSTRAINT if EXISTS "accounts_owner_fkey";
+DROP CONSTRAINT if EXISTS "accounts_owner_fkey";
 
 DROP TABLE if exists "users";
