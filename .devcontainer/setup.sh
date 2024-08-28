7b4bf35f47e50ccca5329e8390efdb09e6f0c963
Hiroki Kondo <kompiro@gmail.com>
2024-08-28 16:28:31 +0900

    feat: Setup ecspresso (#31)

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -25,3 +25,16 @@ exts=(
 for ext in "${exts[@]}"; do
   /home/vscode/.asdf/shims/gh extension install "$ext"
 done
+
+# shellcheck disable=SC1091
+. /home/vscode/.asdf/asdf.sh
+
+# install ecspresso
+asdf plugin add ecspresso
+asdf install ecspresso 2.3.0
+asdf global ecspresso 2.3.0
+
+# install aws cli v2
+asdf plugin add awscli
+asdf install awscli 2.17.39
+asdf global awscli 2.17.39
