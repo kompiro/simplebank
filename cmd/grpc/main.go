b7354258023d5bb901b4729998b996d9a41e4b31
Hiroki Kondo <kompiro@gmail.com>
2024-08-30 20:14:19 +0900

    feat: Enable to launch grpc (#39)

---
+++ cmd/grpc/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	db "github.com/techschool/simplebank/db/sqlc"
+	"github.com/techschool/simplebank/gapi"
+	"github.com/techschool/simplebank/pb"
+	"github.com/techschool/simplebank/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: " + err.Error())
+	}
+	pgxConfig, err := pgxpool.ParseConfig(config.DBSource)
+	if err != nil {
+		return
+	}
+
+	err = util.SetupLogger(pgxConfig)
+	if err != nil {
+		log.Fatal("cannot setup logger: " + err.Error())
+	}
+
+	connPool, err := pgxpool.NewWithConfig(context.Background(), pgxConfig)
+	if err != nil {
+		log.Fatal("cannot connect to db: " + err.Error())
+	}
+
+	store := db.NewStore(connPool)
+	runGrpcServer(config, store)
+
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot start server: " + err.Error())
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener: " + err.Error())
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server: " + err.Error())
+	}
+}
