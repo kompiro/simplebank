351823264078af46a5bab0babc828adbd8890090
Hiroki Kondo <kompiro@gmail.com>
2024-08-30 23:04:16 +0900

    feat: Implement unimplemented rpcs (#40)

--- api/user.go
+++ api/user.go
@@ -35,7 +35,7 @@ func newUserResponse(user db.User) userResponse {
 	}
 }
 
-func (server *Server) CreateUser(ctx *gin.Context) {
+func (server *Server) createUser(ctx *gin.Context) {
 	var req createUserRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
---
+++ gapi/rpc_create_user.go
@@ -0,0 +1,39 @@
+package gapi
+
+import (
+	"context"
+
+	db "github.com/techschool/simplebank/db/sqlc"
+	"github.com/techschool/simplebank/pb"
+	"github.com/techschool/simplebank/util"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	hashedPassword, err := util.HashPassword(req.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to hash a password: %s", err)
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.GetUsername(),
+		HashedPassword: hashedPassword,
+		FullName:       req.GetFullName(),
+		Email:          req.GetEmail(),
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		errCode := db.ErrorCode(err)
+		if errCode == db.UniqueViolation {
+			return nil, status.Errorf(codes.AlreadyExists, "username already exists: %s", err)
+		}
+		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+	rsp := &pb.CreateUserResponse{
+		User: convertUser(user),
+	}
+
+	return rsp, nil
+}
